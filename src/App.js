bfee6bb008d4e3493ee7e01c7c37e507cf120a26
git <rkd8923@gmail.com>
2021-07-01 16:12:04 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Canvas, useLoader } from "@react-three/fiber";
+import React, { Suspense, useState } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import "./App.css";
 
-function App() {
+const Model = (props) => {
+  const { position, scale } = props;
+  const gltf = useLoader(
+    GLTFLoader,
+    "https://firebasestorage.googleapis.com/v0/b/fir-584ce.appspot.com/o/Buggy.glb?alt=media&token=57bd842d-5b75-4c78-984e-5e3854da532d"
+  );
+  console.log(gltf);
+  return (
+    <>
+      <primitive object={gltf.scene} scale={scale} />
+    </>
+  );
+};
+const Model2 = (props) => {
+  const { position, scale } = props;
+  const gltf = useLoader(
+    GLTFLoader,
+    "https://firebasestorage.googleapis.com/v0/b/fir-584ce.appspot.com/o/SheenChair.glb?alt=media&token=297c2f65-a9e7-4d84-9e3d-666730ad7e38"
+  );
+  console.log(gltf);
+  return (
+    <>
+      <primitive object={gltf.scene} scale={scale} />
+    </>
+  );
+};
+
+export default function App() {
+  const [button, setButton] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="button" onClick={() => setButton((prev) => !prev)}>
+        click
+      </div>
+      <Canvas>
+        <Suspense fallback={null}>
+          <group>
+            <group
+              position={[-2, 0, -2]}
+              onPointerOver={() => console.log("hrere")}
+            >
+              <Model scale={0.01} />
+            </group>
+            {button && (
+              <group position={[2, 0, 0]}>
+                <Model2 scale={2} />
+              </group>
+            )}
+          </group>
+          <OrbitControls />
+          <Environment preset="sunset" />
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
-
-export default App;
